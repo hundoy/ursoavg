1242eb19474560a0ea63030eecbad4231ef39c4c
hundoy <zoharwolf@qq.com>
2016-03-26 17:03:06 +0800

    create project

---
+++ android/gen/com/urso/avg/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.urso.avg;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ android/src/com/urso/avg/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.urso.avg;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.urso.avg.UrsoAvgGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new UrsoAvgGame(), config);
+	}
+}
